b55deba0e9d83f6edf852ec0d48fa885912f295e
HRiver <sanscout@naver.com>
2024-02-09 19:48:26 +0900

    #3 create UserVO

--- src/VO/UserVO.java
+++ src/VO/UserVO.java
@@ -1,4 +1,17 @@
 package VO;
 
+
+import lombok.Data;
+
+@Data
 public class UserVO {
+    private int userID;
+    private int userType;
+    private String ID;
+    private String userName;
+    private String taxID;
+    private String addressCity;
+    private int addressNum;
+    private boolean approval;
+
 }
